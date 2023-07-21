4c62254cc37e5f8af4f92046d2b2bb3e000ae936
Tafadzwa Ben <tbmukwepa@gmail.com>
2023-07-21 14:44:44 +0200

    reduced lat and lon

--- weather.py
+++ weather.py
@@ -8,8 +8,11 @@ api_key = os.getenv('API_KEY')
 
 
 def get_lat_lon(city_name, state_code, country_code, api_keys):
-    resp = requests.get(f'http://api.openweathermap.org/geo/1.0/direct?q={city_name},{state_code},{country_code}&appid={api_keys}')
-    print(resp)
+    resp = requests.get(f'http://api.openweathermap.org/geo/1.0/direct?q={city_name},{state_code},{country_code}&appid={api_keys}').json()
+    data = resp[0]
+    lat, lon = data.get('lat'), data.get('lon')
+    return lat, lon
 
 
-get_lat_lon('Toronto', 'ON', 'Canada', api_key)
+
+print(get_lat_lon('Toronto', 'ON', 'Canada', api_key))
