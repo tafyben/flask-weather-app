178dda94a9c558fb4abc6724685cbed392a4d9d6
Tafadzwa Ben <tbmukwepa@gmail.com>
2023-07-21 14:24:00 +0200

    fixed the .env

--- weather.py
+++ weather.py
@@ -1,10 +1,15 @@
 import requests
 from dotenv import load_dotenv
+
 import os
 
 load_dotenv()
 api_key = os.getenv('API_KEY')
 
-def get_lat_lon(city_name, state_code, country_code, API_key):
-    resp = requests.get(
-        f'http://api.openweathermap.org/geo/1.0/direct?q={city_name},{state_code},{country_code}&appid={API_Key}')
+
+def get_lat_lon(city_name, state_code, country_code, api_keys):
+    resp = requests.get(f'http://api.openweathermap.org/geo/1.0/direct?q={city_name},{state_code},{country_code}&appid={api_keys}')
+    print(resp)
+
+
+get_lat_lon('Toronto', 'ON', 'Canada', api_key)
